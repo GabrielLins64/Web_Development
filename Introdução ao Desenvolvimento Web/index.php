0c07d20361584ec9e385e24b516478fd20c1fa55
data-hunter <gabrielinsmelo@gmail.com>
2020-06-13 19:16:12 -0300

    Changed php files to html

--- Introdução ao Desenvolvimento Web/index.php
+++ Introdução ao Desenvolvimento Web/index.php
@@ -6,29 +6,29 @@ if(isset($_GET['i'])){
 	$pagina = addslashes($_GET['i']);
 }
 
-include 'HTML5 e CSS3/header.php';
+include 'HTML5 e CSS3/header.html';
 
 switch ($pagina) {
 	case 'home':
-		include 'home.php';
+		include 'home.html';
 		break;
 
 	case 'html_css':
-		include 'HTML5 e CSS3/html_index.php';
+		include 'HTML5 e CSS3/html_index.html';
 		break;
 
 	case 'javascript':
-		include 'JavaScript/js_index.php';
+		include 'JavaScript/js_index.html';
 		break;
 
 	case 'php':
-		include 'PHP/php_index.php';
+		include 'PHP/php_index.html';
 		break;
 	
 	default:
-		include 'home.php';
+		include 'home.html';
 		break;
 }
 
 /* Carrega o footer.php */
-include 'HTML5 e CSS3/footer.php';
+include 'HTML5 e CSS3/footer.html';
